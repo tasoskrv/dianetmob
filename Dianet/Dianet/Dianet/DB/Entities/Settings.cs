70729ba8a8b7ef0a6284b8f8411d077c54bfde78
spyros <spyros@localhost>
2016-07-24 12:13:02 +0300

    DB

--- Dianet/Dianet/Dianet/DB/Entities/Settings.cs
+++ Dianet/Dianet/Dianet/DB/Entities/Settings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace Dianet.DB.Entities
 {
-    class Settings
+    public class Settings
     {
+        [PrimaryKey]
+        public int IDSettings { get; set; }
+
+        public int TrialPeriod { get; set; }
+
+        public int RemindWeight { get; set; }
     }
 }
