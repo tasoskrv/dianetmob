1e5b8e96d9890a370e0b6573faf6fc3d3619c514
spyros <spyros@localhost>
2016-09-05 22:46:50 +0300

    ok

---
+++ Dianet/Dianet/Dianet/Service/ServiceConnector.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dianet.Service
+{
+    public static class ServiceConnector
+    {
+        private static string BaseUrl = "http://dianet.cloudocean.gr/api/v1";
+        private static HttpClient client = new HttpClient();
+        public static async Task<T> GetServiceData<T>(string url)
+        {
+            var uri = new Uri(BaseUrl + url);
+            var response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(content);
+            }
+            return default(T);
+        }
+    }
+}
