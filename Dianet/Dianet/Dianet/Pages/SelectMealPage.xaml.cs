0cffc39ab5c612298f22134b04bbdd0a0c57645d
Thanos_K <Thanos_K@localhost>
2016-09-18 15:27:58 +0300

    selectPage (not functional)

--- Dianet/Dianet/Dianet/Pages/SelectMealPage.xaml.cs
+++ Dianet/Dianet/Dianet/Pages/SelectMealPage.xaml.cs
@@ -11,8 +11,33 @@ namespace Dianet.Pages
     public partial class SelectMealPage : ContentPage
     {
         public SelectMealPage()
-        {
+        {           
             InitializeComponent();
+        }
+
+        void OnUnitChosen(object sender, EventArgs e)
+        {
+            Picker unitPicker = (Picker)sender;
+
+            if (unitPicker.SelectedIndex > 1)
+            {
+                counter1Picker.IsEnabled = true;
+                counter2Picker.IsEnabled = true;
+                counter1Picker.SelectedIndex = 1;
+                counter2Picker.SelectedIndex = 1;
+            }
+            else
+            {
+                counter1Picker.SelectedIndex = 0;
+                counter2Picker.SelectedIndex = 0;
+                counter1Picker.IsEnabled = false;
+                counter2Picker.IsEnabled = false;
+            }            
+        }
+
+        async void OnChooseBtnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Myday());
         }
     }
 }
