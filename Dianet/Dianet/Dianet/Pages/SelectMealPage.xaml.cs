f1545f9f8470dd1b6ce57884d58b83c71689a9d3
thanos <thanos@localhost>
2016-10-17 01:09:53 +0300

    Select Page

--- Dianet/Dianet/Dianet/Pages/SelectMealPage.xaml.cs
+++ Dianet/Dianet/Dianet/Pages/SelectMealPage.xaml.cs
@@ -15,7 +15,6 @@ namespace Dianet.Pages
     public partial class SelectMealPage : ContentPage
     {
        private SQLiteConnection conn = null;
-
         // private ObservableCollection<Unit> Units = new ObservableCollection<Unit>();
         Dictionary<string, MealUnit> DicUnit = new Dictionary<string, MealUnit>();
         public int IDMealSelected{ get; set; }
@@ -32,13 +31,14 @@ namespace Dianet.Pages
             unitPicker.Items.Clear();
             //SELECT M.idMealUnit, M.IdUnit, M.Calories FROM MealUnit M, Unit U where M.idUnit= U.idUnit -- Κανονικο query
             // IEnumerable<MealUnit> UnitList = conn.Query<MealUnit>("SELECT M.idMealUnit, M.IdUnit, M.Calories, U.Name FROM MealUnit M, Unit U ");
-            IEnumerable<Unit> UnitList = conn.Query<Unit>("SELECT idUnit, Name FROM Unit");
-            IEnumerable<MealUnit> Mealunit = conn.Query<MealUnit>("SELECT idMealUnit, idUnit, idMeal, Calories, Protein, Carb, Fat  FROM MealUnit");
-            foreach (Unit unit in UnitList)
+           //IEnumerable<Unit> UnitList1 = conn.Query<Unit>("SELECT idUnit, Name FROM Unit");
+           // IEnumerable<MealUnit> Mealunit = conn.Query<MealUnit>("SELECT idMealUnit, idUnit, idMeal, Calories, Protein, Carb, Fat  FROM MealUnit");
+           // IEnumerable<Meal> meal = conn.Query<Meal>("SELECT idMeal, Name, Description, idLang, Fertility FROM Meal");
+           IEnumerable<MealUnit> UnitList = conn.Query<MealUnit>("SELECT M.idMealUnit, M.IdUnit, M.Calories, M.Protein, M.Carb, M.Fat, U.Name as Name FROM MealUnit as M, Unit as U WHERE IDMeal="+this.IDMealSelected);
+            foreach (MealUnit mealunit in UnitList)
             {
-                unitPicker.Items.Add(unit.Name);
-               // DicUnit.Add(mealunit.Name, mealunit); // ο Πινακας mealUnit θα χρησιμοποιηθει για να ενημερωθει ο usermeal
-
+                unitPicker.Items.Add(mealunit.Name);
+                DicUnit.Add(mealunit.Name, mealunit); 
             }
             
         }
@@ -48,33 +48,41 @@ namespace Dianet.Pages
         void OnUnitChosen(object sender, EventArgs e)
         {
             Picker unitPicker = (Picker)sender;
-
             if (unitPicker.SelectedIndex >= 0)
             {
                string Name = unitPicker.Items[unitPicker.SelectedIndex];
-                // MealUnit mealUnit = DicUnit[Name];
-
-                // DisplayAlert("You selected", mealUnit.Name, "OK");
-                DisplayAlert("You selected", Name, "OK");
-
-
-                counter1Picker.IsEnabled = true;
-                counter2Picker.IsEnabled = true;
-                counter1Picker.SelectedIndex = 1;
-                counter2Picker.SelectedIndex = 1;
+                MealUnit mealUnit = DicUnit[Name];
+                //DisplayAlert("You selected", mealUnit.Name, "OK");
+                counter1Picker.IsEnabled = true;       
             }
             else
-            {
-                counter1Picker.SelectedIndex = 0;
-                counter2Picker.SelectedIndex = 0;
+            {               
                 counter1Picker.IsEnabled = false;
-                counter2Picker.IsEnabled = false;
             }            
         }
 
-        async void OnChooseBtnClicked(object sender, EventArgs e)
+        void OnChosen1(object sender, EventArgs e)
         {
-            await Navigation.PopToRootAsync();
+            Picker unitPicker = (Picker)sender;
+            if (unitPicker.SelectedIndex >= 0)
+            {            
+                counter2Picker.IsEnabled = true;
+            }           
+        }
+
+        void OnChosen2(object sender, EventArgs e)
+        {
+            Picker unitPicker = (Picker)sender;
+            if (unitPicker.SelectedIndex >= 0)
+            {
+                DisplayAlert("Quantity Calculation on process...", "", "OK");
+            }
+            }
+
+        async void OnChooseBtnClicked(object sender, EventArgs e)
+        { 
+
+            await Navigation.PopToRootAsync(true);
             
         }
     }
