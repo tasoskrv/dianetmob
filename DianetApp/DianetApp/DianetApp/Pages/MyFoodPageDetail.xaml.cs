c2df449e7e19bb337b3bc0f811cfbea914c320ea
akv <akv@localhost>
2016-11-14 15:18:34 +0200

    myfood

---
+++ DianetApp/DianetApp/DianetApp/Pages/MyFoodPageDetail.xaml.cs
@@ -0,0 +1,56 @@
+﻿using DianetApp.DB;
+using DianetApp.DB.Entities;
+using SQLite;
+using System;
+
+using Xamarin.Forms;
+
+namespace DianetApp.Pages
+{
+    public partial class MyFoodPageDetail : ContentPage
+    {
+        private UserFood uFood;
+        private SQLiteConnection conn = null;
+
+        public MyFoodPageDetail()
+        {
+            InitializeComponent();
+            conn = StorageManager.GetConnection();
+        }
+
+        public void LoadData(int IDUserfood = 0)
+        {
+            if (IDUserfood > 0)
+                uFood = conn.Get<UserFood>(IDUserfood);
+            else
+            {
+                uFood = new UserFood();
+                uFood.IDUser = StorageManager.GetConnectionInfo().LoginUser.IDUser;
+            }
+            BindingContext = uFood;
+        }
+
+        private void OnSaveFoodClicked(object sender, EventArgs e)
+        {
+            uFood.UpdateDate = DateTime.UtcNow;
+            if (uFood.Name.Equals(""))
+                DisplayAlert("Please", "fill name", "OK");
+            else if (uFood.Description.Equals(""))
+            {
+                DisplayAlert("Please", "fill description", "OK");
+            }
+            else if (uFood.IDUserFood > 0)
+            {
+                StorageManager.UpdateData(uFood);
+                Navigation.PopAsync();
+            }
+            else
+            {
+                uFood.InsertDate = uFood.UpdateDate;
+                StorageManager.InsertData(uFood);
+                Navigation.PopAsync();
+            }
+        }
+
+    }
+}
