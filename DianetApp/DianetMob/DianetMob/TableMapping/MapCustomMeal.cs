844e93ef26cba68791157c5a76540485296ef844
akg <akg@localhost>
2017-01-10 16:50:49 +0200

    !

---
+++ DianetApp/DianetMob/DianetMob/TableMapping/MapCustomMeal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DianetMob.TableMapping
+{
+    public class MapCustomMeal
+    {
+        public int IDMeal { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int IDUser { get; set; }
+        public int IdMealUnit { get; set; }
+        public double Calories { get; set; }
+        public double Carb { get; set; }
+        public double Fiber { get; set; }
+        public double Natrium { get; set; }
+        public double Potassium { get; set; }
+        public double Fat { get; set; }
+        public double SatFat { get; set; }
+        public double Sugar { get; set; }
+        public double UnSatFat { get; set; }
+        public double Cholesterol { get; set; }
+
+        public double Protein { get; set; }
+        public int IDServer { get; set; }
+
+    }
+}
